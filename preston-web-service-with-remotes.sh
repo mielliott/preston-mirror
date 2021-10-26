0cefa2a62c2c228dbba31ac83c4e0240ba7a4c67
Michael Elliott <mielliott@ufl.edu>
2021-10-26 15:25:38 +0000

    Use custom remotes

--- preston-web-service-with-remotes.sh
+++ preston-web-service-with-remotes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/var/lib/preston-service/preston-web-service.sh "file:///home/preston/preston-gib/data,file:///home/preston/preston-bhl/data,file:///home/preston/preston-dataone/data"
+/var/lib/preston-service/preston-web-service.sh "file:///mnt/preston.acis.ufl.edu_data/gbif-idigbio-biocase/data/,file:///mnt/preston.acis.ufl.edu_data/gbif-idigbio-biocase/bhl/data/,file:///mnt/preston.acis.ufl.edu_data/gbif-idigbio-biocase/dataone/data/"
